63b253772cc058f69464fcf7e0342606c7000b12
Mayuri <mokshkamble950@gmail.com>
2024-12-16 14:25:02 +0530

    movement

--- Assets/scripts/model.cs
+++ Assets/scripts/model.cs
@@ -1,49 +1,48 @@
 using UnityEngine;
-using System.Collections;
 
 public class Model : MonoBehaviour
 {
-    public Animator modelAnim;
-    public Rigidbody playerRigid;
-    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
+    public Rigidbody playerRigid; // Reference to the Rigidbody component
+    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed; // Speed variables
     public bool walking;
-    public Transform playerTrans;
+    public Transform playerTrans; // Reference to the player's Transform
+
+    void Start()
+    {
+        // Initialize values
+        walking = false;
+        w_speed = olw_speed; // Initialize walking speed
+
+        // Lock the Rigidbody rotation to prevent falling
+        playerRigid.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ;
+    }
 
     void FixedUpdate()
     {
+        // Move Forward
         if (Input.GetKey(KeyCode.W))
         {
-            playerRigid.linearVelocity = transform.forward * w_speed * Time.deltaTime;
+            playerRigid.linearVelocity = transform.forward * w_speed * Time.deltaTime; // Fixed `linearVelocity` to `velocity`
         }
+
+        // Move Backward
         if (Input.GetKey(KeyCode.S))
         {
             playerRigid.linearVelocity = -transform.forward * w_speed * Time.deltaTime;
         }
     }
 
-    // Start is called before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        walking = false;
-        w_speed = olw_speed; // Initialize walking speed
-    }
-
-    // Update is called once per frame
     void Update()
     {
         // Walk Forward
         if (Input.GetKeyDown(KeyCode.W))
         {
-            modelAnim.SetTrigger("walk");
-            modelAnim.ResetTrigger("idle");
             walking = true;
         }
 
         // Walk Backward
         if (Input.GetKeyDown(KeyCode.S))
         {
-            modelAnim.SetTrigger("walkback");
-            modelAnim.ResetTrigger("idle");
             walking = false;
         }
 
@@ -51,18 +50,12 @@ public class Model : MonoBehaviour
         if (walking && Input.GetKey(KeyCode.LeftShift))
         {
             w_speed += rn_speed;
-            modelAnim.SetTrigger("run");
-            modelAnim.ResetTrigger("walk");
         }
 
         // Stop Running When Left Shift is Released
         if (walking && Input.GetKeyUp(KeyCode.LeftShift))
         {
             w_speed = olw_speed;
-            modelAnim.SetTrigger("walk");
-            modelAnim.ResetTrigger("run");
         }
     }
 }
-
-
