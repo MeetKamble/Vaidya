3486015cc46c6978966fbe858b492d8eb61d7d99
Mayuri <mokshkamble950@gmail.com>
2024-12-16 13:17:42 +0530

    may

--- Assets/scripts/model.cs
+++ Assets/scripts/model.cs
@@ -1,54 +1,68 @@
 using UnityEngine;
 using System.Collections;
-using System.Collections.Generic;
 
-public class model : MonoBehaviour
+public class Model : MonoBehaviour
 {
     public Animator modelAnim;
     public Rigidbody playerRigid;
-    public float w_speed, wb_speed, olw_speed, run_speed, ro_speed;
+    public float w_speed, wb_speed, olw_speed, rn_speed, ro_speed;
     public bool walking;
-    public Transform palyerTrans;
-    
+    public Transform playerTrans;
+
     void FixedUpdate()
     {
-        if (Input.GetKey(KeyCode.W){
-            playerRigid.angularVelocity = transform.forward * w_speed * Time.deltaTime;
-
+        if (Input.GetKey(KeyCode.W))
+        {
+            playerRigid.linearVelocity = transform.forward * w_speed * Time.deltaTime;
         }
-        if (Input.GetKey(KeyCode.S){
-            playerRigid.angularVelocity = -transform.forward * w_speed * Time.deltaTime;
-
+        if (Input.GetKey(KeyCode.S))
+        {
+            playerRigid.linearVelocity = -transform.forward * w_speed * Time.deltaTime;
         }
     }
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    // Start is called before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        walking = false;
+        w_speed = olw_speed; // Initialize walking speed
     }
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.W))
-        {
-            modelAnim.setTrigeer("walk");
-            modelAnim.ResetTrigger("idle");
-            walking = true;
-        //steps1.setActive(true);
-      }
+        // Walk Forward
         if (Input.GetKeyDown(KeyCode.W))
         {
             modelAnim.SetTrigger("walk");
             modelAnim.ResetTrigger("idle");
-            walking = false;
-            //steps1.setActive(false);
+            walking = true;
         }
+
+        // Walk Backward
         if (Input.GetKeyDown(KeyCode.S))
         {
             modelAnim.SetTrigger("walkback");
             modelAnim.ResetTrigger("idle");
-            //steps1.setActive(false);
+            walking = false;
+        }
+
+        // Run When Left Shift is Pressed
+        if (walking && Input.GetKey(KeyCode.LeftShift))
+        {
+            w_speed += rn_speed;
+            modelAnim.SetTrigger("run");
+            modelAnim.ResetTrigger("walk");
+        }
+
+        // Stop Running When Left Shift is Released
+        if (walking && Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            w_speed = olw_speed;
+            modelAnim.SetTrigger("walk");
+            modelAnim.ResetTrigger("run");
         }
     }
 }
+
+
